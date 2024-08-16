3a3b4ac97cd028b315291e5608701fa69d72e662
ivan ivaneev <ivaneev.ivane@gmail.com>
2024-08-16 15:29:02 +0300

    add hyper support

---
+++ Coffee/Pages/News.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coffee.Pages
+{
+    public class NewsModel : PageModel
+    {
+        private readonly ILogger<NewsModel> _logger;
+
+        public NewsModel(ILogger<NewsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
